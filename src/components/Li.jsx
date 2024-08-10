ab26ffa2893245437cfae6997625e2f2dd94677d
Ismail Ismaili Alaoui <i.alaoui@labelvie.ma>
2024-08-10 15:49:59 +0100

    initiate mondial sampling

--- src/components/Li.jsx
+++ src/components/Li.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Li = ({ title }) => {
     return (<li>
         <Link
-            to={"/services"}
+            to={"#"}
             className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100
            hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 
            lg:hover:text-teal-700 lg:p-0 dark:text-gray-400
